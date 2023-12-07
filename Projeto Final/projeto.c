24438363ddbfca940f070d3a99e3cf19c2b83ac5
GABRIEL GUARNIERI GOMES <gabrielguarnierigomes@gmail.com>
2023-12-07 18:50:24 -0300

    Projeto final Finalizado

--- Projeto Final/projeto.c
+++ Projeto Final/projeto.c
@@ -52,6 +52,50 @@ int evaluateExpression(Node *root)
     }
 }
 
+//Função para Criar uma árvore binária a partir de uma equação com 
+//Notação Polonesa Invertida
+struct Node *createTreeFromExpression(const char *expression, int *pos)
+{
+    struct Node *root = NULL;
+
+    // Ignorar espaços em branco
+    while (expression[*pos] == ' ')
+    {
+        (*pos)++;
+    }
+
+  
+    if (expression[*pos] == '(')
+    {
+        (*pos)++; 
+
+       
+        root = createNode(expression[*pos]);
+        (*pos)++; 
+
+     
+        root->left = createTreeFromExpression(expression, pos);
+        root->right = createTreeFromExpression(expression, pos);
+
+       
+        (*pos)++;
+
+      
+        if (expression[*pos] == ')')
+        {
+            (*pos)++;
+        }
+    }
+    else
+    {
+     
+        root = createNode(expression[*pos]);
+        (*pos)++; 
+    }
+
+    return root;
+}
+
 // Função para imprimir a expressão na notação infixada
 void printInfix(Node *root)
 {
@@ -76,10 +120,6 @@ void inorderTraversal(struct Node *root)
 {
     if (root != NULL)
     {
-        // Preferencia por nós da Esquerda
-        // Começa com o "Galho" Da Esquerda e depois vai para a direita se existir
-        // Vai para esquerda, se não tiver vai para a direita
-        // Vai e Volta
         inorderTraversal(root->left);
         printf("%d ", root->data);
         inorderTraversal(root->right);
@@ -99,20 +139,11 @@ void freeTree(Node *root)
 
 int main()
 {
-    // Construindo a árvore para a expressão (+ (+ 5 3) (* 2 4))
-    Node *root = createNode('+');
-    root->left = createNode('+');
-    root->left->left = createNode('5');
-    root->left->right = createNode('3');
-    root->right = createNode('*');
-    root->right->left = createNode('2');
-    root->right->right = createNode('4');
+   
+    const char *expression1 = "(- (* 2 (+ 3 5)) (/ 8 2))";
 
-
-    // int index = 0;
-    // char equacao[] = "(+ (+ 5 3) (* 2 4))";
-
-    // Node *tree = buildTree(equacao, &index);
+    int pos = 0;
+    struct Node *root = createTreeFromExpression(expression1, &pos);
 
     // Avaliando e imprimindo a expressão
     printf("Expressao Infixada: ");
